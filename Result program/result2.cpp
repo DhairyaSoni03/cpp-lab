fdb61b2123f3237461d4c9f57796cb6a7402a799
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2026-01-22 17:47:31 +0530

    Add files via upload

---
+++ Result program/result2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    cout<<"Name"<<setw(10)<<": Dhairya soni"<<setw(40)<<"ID NO."<<setw(10)<<":25ce119"<<endl;
+    cout<<"Programme"<<setw(10)<<":BTECH CE"<<endl;
+    cout<<"Month&year"<<setw(10)<<": December 2025"<<setw(35)<<"Semester"<<setw(3)<<":1"<<endl;
+
+    cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"Course"<<"\tCredits(C)"<<"\tCCE(theory)"<<"\tSEE(theory)"<<"\tCCE(practical)"<<"\tSEE(practical)"<<"\tGrade"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"CEUC101"<<"\t5.00"<<"\t\tB+"<<"\t\tA"<<"\t\tB+"<<"\t\tB+"<<"\t\tB+"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"EEUD101"<<"\t4.00"<<"\t\tc"<<"\t\tp"<<"\t\tB"<<"\t\tB+"<<"\t\tc"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"FTUC101"<<"\t--"<<"\t\t--"<<"\t\t--"<<"\t\t--"<<"\t\tA+"<<"\t\tA+"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"HSUA101"<<"\t2.00"<<"\t\t--"<<"\t\t--"<<"\t\tA"<<"\t\tA"<<"\t\tA"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"ITUS101"<<"\t2.00"<<"\t\t--"<<"\t\t--"<<"\t\tB+"<<"\t\tB+"<<"\t\tB+"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"MSUD101"<<"\t4.00"<<"\t\tC"<<"\t\tP"<<"\t\t--"<<"\t\t--"<<"\t\tP"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"PSUD101"<<"\t2.00"<<"\t\t--"<<"\t\t--"<<"\t\tB"<<"\t\tP"<<"\t\tB"<<endl;
+     cout<<" "<<endl;
+    cout<<setfill('-')<<setw(100)<<0<<endl;
+    cout<<"\t\tSemester performance"<<"\t\t Total Credits"<<"\t\tCredits Earned"<<"\t\tSGPA"<<endl;
+    cout<<" "<<endl;
+    cout<<"\t\t\t"<<"\t\t\t 19.00"<<"\t\t\t19.00"<<"\t\t\t5.95"<<endl;
+
+    return 0;
+}
