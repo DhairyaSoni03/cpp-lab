320b0edade578b752a700a923f8bc0d39fc5ea3a
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2026-02-23 22:58:55 +0530

    p1.2.cpp

--- 1.2/p1.2.cpp
+++ 1.2/p1.2.cpp
@@ -9,66 +9,67 @@ public:
     string author;
     int copies;
 
-    void addBook() {
-        cout << "Enter Book ID: ";
-        cin >> bookID;
-
-        cout << "Enter Title : ";
-        cin >> title;
-
-        cout << "Enter Author Name (no spaces): ";
-        cin >> author;
-
-        cout << "Enter Number of Copies: ";
-        cin >> copies;
+    void addBook() 
+{
+        cout<<"Enter Book ID: ";
+        cin>>bookID;
+        cout<<"Enter Title : ";
+        cin>>title;
+        cout<<"Enter Author Name (no spaces): ";
+        cin>>author;
+        cout<<"Enter Number of Copies: ";
+        cin>>copies;
     }
-
-    void issueBook() {
-        if (copies > 0) {
+    void issueBook() 
+{
+        if (copies>0) 
+        {
             copies--;
-            cout << "Book issued successfully\n";
-        } else {
-            cout << "No copies available\n";
+            cout<<"Book issued successfully\n";
+        } else 
+        {
+            cout<<"No copies available\n";
         }
     }
 
-    void returnBook() {
+    void returnBook() 
+{
         copies++;
-        cout << "Book returned successfully\n";
+        cout<<"Book returned successfully\n";
     }
 
     void displayBook() {
-        cout << bookID << " | " << title << " | "
-             << author << " | Copies: " << copies << endl;
+        cout<<bookID<<" | "<<title<<" | "
+             <<author<<" | Copies: "<<copies<<endl;
     }
 };
 
-int main() {
+int main() 
+{
     Book books[10];
     int count = 0;
     int choice = 0;
     int id;
-
     while (choice != 5) {
-        cout << "\n--- Library Menu ---\n";
-        cout << "1. Add Book\n";
-        cout << "2. Issue Book\n";
-        cout << "3. Return Book\n";
-        cout << "4. Display Books\n";
-        cout << "5. Exit\n";
-        cout << "Enter choice: ";
-        cin >> choice;
-
-        switch (choice) {
+        cout<<"\nLibrary Menu\n";
+        cout<<"1. Add Book\n";
+        cout<<"2. Issue Book\n";
+        cout<<"3. Return Book\n";
+        cout<<"4. Display Books\n";
+        cout<<"5. Exit\n";
+        cout<<"Enter choice: ";
+        cin>>choice;
+        switch (choice) 
+        {
         case 1:
             books[count].addBook();
             count++;
             break;
-
         case 2:
-            cout << "Enter Book ID to issue: ";
-            cin >> id;
-            for (int i = 0; i < count; i++) {
+            cout<<"Enter Book ID to issue: ";
+            cin>>id;
+            for(int i = 0; i < count; i++) 
+            {
                 if (books[i].bookID == id) {
                     books[i].issueBook();
                     break;
@@ -77,31 +78,32 @@ int main() {
             break;
 
         case 3:
-            cout << "Enter Book ID to return: ";
-            cin >> id;
-            for (int i = 0; i < count; i++) {
-                if (books[i].bookID == id) {
+            cout<<"Enter Book ID to return: ";
+            cin>>id;
+            for (int i = 0; i < count; i++) 
+            {
+                if (books[i].bookID == id) 
+                {
                     books[i].returnBook();
                     break;
                 }
             }
             break;
-
         case 4:
-            cout << "\nLibrary Books:\n";
-            for (int i = 0; i < count; i++) {
+            cout<<"\nLibrary Books:\n";
+            for (int i = 0; i < count; i++) 
+            {
                 books[i].displayBook();
             }
             break;
-
         case 5:
-            cout << "Exiting program...\n";
+            cout<<"Exiting program...\n";
             break;
-
         default:
-            cout << "Invalid choice\n";
+            cout<<"Invalid choice\n";
         }
     }
 
     return 0;
 }
+
